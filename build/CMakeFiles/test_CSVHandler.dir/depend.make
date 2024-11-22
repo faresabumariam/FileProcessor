03fd5dcdfd01a1217f588b461820170f1bb5d251
Fares <faresabumariam@hotmail.com>
2024-11-22 13:56:21 +0100

    Remove ignored files from repository

--- build/CMakeFiles/test_CSVHandler.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for test_CSVHandler.
-# This may be replaced when dependencies are built.
--- include/CSVHandlerFactory.h
+++
@@ -1,9 +0,0 @@
-#include "FileHandlerFactory.h"
-#include "CSVHandler.h"
-
-class CSVHandlerFactory : public FileHandlerFactory {
-public:
-    std::unique_ptr<FileHandler> createHandler() const override {
-        return std::make_unique<CSVHandler>();
-    }
-};
